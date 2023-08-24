4f0b48ab14ba5256f201d5d7d77203c2a91aca91
sourya-code <86978046+sourya-code@users.noreply.github.com>
2023-08-24 17:36:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+song = "";
+song1 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+    song1 = loadSound("music1.mp3")
+}
+
+function setup()
+{
+    canvas = createCanvas(1000,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " +  scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " +  scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY " + rightWristY);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+
+function draw()
+{
+    image(video , 0, 0, 600, 500)
+
+    fill("#FF0000");
+    stroke("FF0000");
+
+if(scoreLeftWrist > 0.2)
+{
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTMl = "Volume = " + volume;
+    song.setVolume(volume);
+}
+
+if(scoreRightWrist > 0.2)
+{
+    circle(rightWristX, rightWristY, 20);
+    InNumberrightWristY = Number(rightWristY);
+    remove_decimals = floor(InNumberrightWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTMl = "Volume = " + volume;
+    song.setVolume(volume);
+}
+}
+
+function play()
+{
+    song.play();
+    song1.play();
+    song.setVolume(1);
+    song1.setVolume(1);
+    song.rate(1);
+    song1.rate(1);
+}
